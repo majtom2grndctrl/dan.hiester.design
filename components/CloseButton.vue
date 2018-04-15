7f05812fff1165abe5fa3ce37880632cb2a6c0dc
Dan Hiester <dan.hiester@gmail.com>
2018-04-15 15:53:08 -0700

    Add close button component to repo.

---
+++ components/CloseButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg width="17px" height="17px" viewBox="0 0 17 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+      <title>Close</title>
+      <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(-299.000000, -365.000000)">
+          <path d="M305.163582,373.284903 L299,367.12132 L301.12132,365 L307.284903,371.163582 L313.276019,365.172466 L315.397339,367.293786 L309.406223,373.284903 L315.263456,379.142136 L313.142136,381.263456 L307.284903,375.406223 L301.255204,381.435922 L299.133883,379.314601 L305.163582,373.284903 Z" fill="currentcolor"></path>
+      </g>
+  </svg>
+</template>
