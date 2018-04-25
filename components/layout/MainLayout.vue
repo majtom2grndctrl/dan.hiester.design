335a1f496d5ce57bc6dec5403b44d47685c8c837
Dan Hiester <dan.hiester@gmail.com>
2018-04-25 00:27:40 -0700

    Introduce main layout component.

---
+++ components/layout/MainLayout.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <header class="header">
+      <nuxt-link to="/" class="logo">
+        <dy-logo />
+        <span>Distantly Yours</span>
+      </nuxt-link>
+      <mobile-nav-launcher/>
+    </header>
+    <slot />
+    <footer class="footer"></footer>
+  </div>
+</template>
+
+
+<script>
+import DyLogo from '~/components/DyLogo.vue'
+import MobileNavLauncher from '~/components/buttons/MobileNavLauncher.vue'
+
+export default {
+  components: {
+    DyLogo,
+    MobileNavLauncher
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+  .header {
+    display: flex;
+      align-content: center;
+      flex-direction: row;
+      justify-content: space-between;
+  }
+</style>
