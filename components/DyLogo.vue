c4bda571e42238b540f0f71d01601fa7522dec03
Dan Hiester <dan.hiester@gmail.com>
2018-04-25 00:27:01 -0700

    Update logo component to use correct dimensions, place incorrect logo directly on home for now.

--- components/DyLogo.vue
+++ components/DyLogo.vue
@@ -1,15 +1,13 @@
 <template>
-  <svg width="71px" height="52px" viewBox="0 0 71 52" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-      <title>dy-home-logo</title>
-      <defs></defs>
-      <g id="Home-Desktop-HD" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(-378.000000, -379.000000)">
-          <g id="Group-2" transform="translate(378.000000, 379.000000)">
-              <g id="dy-home-logo">
-                  <path d="M28.7999983,48.0487203 L28.7999983,26.200067 L40.8337847,18.8270028 C41.5924892,21.1626926 41.9999975,23.6376088 41.9999975,26.1999983 C41.9999975,35.4181561 36.7260667,43.5042436 28.7999983,48.0487203 Z" id="Combined-Shape" fill="#00B3EE" style="mix-blend-mode: color-burn;"></path>
-                  <path d="M13.9999992,51.9331998 L2.84217094e-14,51.9331998 L2.84217094e-14,0.466796875 L13.9999992,0.466796875 C29.4639712,0.466796875 41.9999975,11.9879436 41.9999975,26.1999983 C41.9999975,40.412053 29.4639712,51.9331998 13.9999992,51.9331998 Z" id="Combined-Shape" fill="#03A1D5" style="mix-blend-mode: color-burn;"></path>
-                  <polygon id="Rectangle-Copy-2" fill="#03A1D5" style="mix-blend-mode: color-burn;" points="28.7999983 26.200067 70.7999958 0.466796875 70.7999958 51.9331998 28.7999983 51.9331998"></polygon>
-              </g>
-          </g>
+  <svg width="46px" height="34px" viewBox="0 0 46 34" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <title>Distantly Yours Logo</title>
+    <style></style>
+    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+      <g>
+        <path class="shape-overlay" d="M18.7088333,30.7511815 L18.7088333,16.7680431 L26.5261291,12.049282 C27.0189929,13.5441235 27.2837153,15.1280699 27.2837153,16.7679992 C27.2837153,22.6676203 23.8577049,27.8427163 18.7088333,30.7511815 Z" fill-opacity="0.600000024" fill="#AEC8D1" style="mix-blend-mode: color-burn;"></path>
+        <path class="shape-d" d="M9.09457175,33.2372483 L2.84217094e-14,33.2372483 L2.84217094e-14,0.298750004 L9.09457175,0.298750004 C19.1401583,0.298750004 27.2837153,7.67228402 27.2837153,16.7679992 C27.2837153,25.8637143 19.1401583,33.2372483 9.09457175,33.2372483 Z" fill-opacity="0.298148777" fill="#03A1D5"></path>
+        <polygon class="shape-y" fill-opacity="0.174450861" fill="#03A1D5" points="18.7088333 16.7680431 45.9925486 0.298750004 45.9925486 33.2372483 18.7088333 33.2372483"></polygon>
       </g>
+    </g>
   </svg>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <template>
   <main class="home-container">
     <header>
-      <dy-logo class="home-logo" />
+      <svg width="71px" height="52px" viewBox="0 0 71 52" style="mix-blend-mode: color-burn;" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <title>dy-home-logo</title>
+        <g id="Home-Desktop-HD" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(-378.000000, -379.000000)">
+          <g id="Group-2" transform="translate(378.000000, 379.000000)">
+            <g id="dy-home-logo">
+              <path d="M28.7999983,48.0487203 L28.7999983,26.200067 L40.8337847,18.8270028 C41.5924892,21.1626926 41.9999975,23.6376088 41.9999975,26.1999983 C41.9999975,35.4181561 36.7260667,43.5042436 28.7999983,48.0487203 Z" id="Combined-Shape" fill="#00B3EE" style="mix-blend-mode: color-burn;"></path>
+              <path d="M13.9999992,51.9331998 L2.84217094e-14,51.9331998 L2.84217094e-14,0.466796875 L13.9999992,0.466796875 C29.4639712,0.466796875 41.9999975,11.9879436 41.9999975,26.1999983 C41.9999975,40.412053 29.4639712,51.9331998 13.9999992,51.9331998 Z" id="Combined-Shape" fill="#03A1D5" style="mix-blend-mode: color-burn;"></path>
+              <polygon id="Rectangle-Copy-2" fill="#03A1D5" style="mix-blend-mode: color-burn;" points="28.7999983 26.200067 70.7999958 0.466796875 70.7999958 51.9331998 28.7999983 51.9331998"></polygon>
+            </g>
+          </g>
+        </g>
+      </svg>
       <h1 class="title" :class="mobileNav && 'mobileNavOpen'">Distantly Yours</h1>
       <div class="subtitle">Push Farther</div>
     </header>
