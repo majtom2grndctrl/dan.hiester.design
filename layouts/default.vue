1b74ab288502ee128a352750725819f70e5bc987
Dan Hiester <dan.hiester@gmail.com>
2018-05-01 00:02:19 -0700

    Initial implementation of internal mobile nav.

--- layouts/default.vue
+++ layouts/default.vue
@@ -20,7 +20,18 @@
               <nuxt-link to="/blog">Blog</nuxt-link>
             </nav>
             <div class="mobile-nav">
-              <mobile-nav-launcher/>
+              <mobile-nav-launcher v-on:launch="mobileNav = !mobileNav" :class="mobileNav && 'mobileNavOpen'" />
+              <transition name="home-nav">
+                <div v-if="mobileNav">
+                  <div class="modal-overlay" v-on:click="mobileNav = !mobileNav" />
+                  <nav class="mobile-nav-links" v-on:click="mobileNav = !mobileNav">
+                    <CloseButton class="mobile-nav-close-button"/>
+                    <h2>Menu</h2>
+                    <nuxt-link to="/portfolio">Portfolio</nuxt-link>
+                    <nuxt-link to="/blog">Blog</nuxt-link>
+                  </nav>
+                </div>
+              </transition>
             </div>
           </header>
           <nuxt/>
@@ -40,6 +51,11 @@ export default {
   components: {
     DyLogo,
     MobileNavLauncher
+  },
+  data() {
+    return {
+      mobileNav: false
+    }
   }
 }
 </script>
@@ -121,4 +137,93 @@ export default {
     outline: none;
   }
 }
+
+
+$mobile-nav-transition-duration: .3s;
+
+.home-nav-enter-active, .home-nav-leave-active {
+  //mix-blend-mode: color-burn;
+  transition: background-color $mobile-nav-transition-duration; // background-color is just there to trigger the transition timing.
+  .modal-overlay {
+    mix-blend-mode: color-burn;
+  }
+  .modal-overlay, .mobile-nav-links {
+    transition: opacity .2s ease-in-out, transform .2s;
+  }
+}
+.home-nav-enter, .home-nav-leave-to {
+  > .modal-overlay, > .mobile-nav-links {
+    opacity: 0;
+  }
+  > .mobile-nav-links {
+    transform: perspective(10em) translate3d(-70%, 20%, -5em);
+  }
+}
+.mobile-nav {
+  @media (min-width: $viewport-small) {
+    display: none;
+  }
+  &-launcher {
+    transition: filter .3s, opacity .3s;
+    &.mobileNavOpen {
+        opacity: .1;
+    }
+  }
+  &-close-button {
+    color: rgba(255, 255, 255, .7);
+    cursor: pointer;
+    position: absolute;
+      top: 1.1em;
+      left: 100%;
+  }
+  &-links {
+    border-left: 1px solid rgba(255, 255, 255, .5);
+    box-sizing: border-box;
+    display: flex;
+      flex-direction: column;
+      align-content: flex-start;
+      justify-content: space-between;
+    font-size: (24rem/16);
+//    height: 55vh;
+    height: 33vh;
+    width: 50vw;
+    padding: .5em 0 1em .5em;
+    position: fixed;
+//      top: 22.5%;
+      top: 33%;
+      left: 50%;
+      z-index: 6;
+    text-align: left;
+    transition: transform $mobile-nav-transition-duration ease-out;
+    transform: perspective(10em) translate3d(-50%, 0, 0);
+    > h2 {
+      color: rgba(255, 255, 255, .66);
+      font-weight: 400;
+      font-size: (16em/24);
+      margin: 0;
+      padding: (16rem/16);
+      text-transform: uppercase;
+    }
+    > a {
+      color: #fff;
+      display: block;
+      padding: .75em;
+      text-decoration: none;
+    }
+  }
+}
+.modal-overlay {
+  background: rgba(0,0,0,.6);
+  @supports (mix-blend-mode: color-burn) {
+//    background: linear-gradient(45deg, rgba(0,32,42,1) 0%, rgba(0,66,88,1) 16%, rgba(0,82,152,1) 39%, rgba(0,82,152,1) 59%, rgba(34,118,190,1) 65%, rgba(0,82,152,1) 73%, rgba(201,242,255,1) 100%);
+//    mix-blend-mode: color-burn;
+  }
+  position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 5;
+}
+
 </style>
