0f83954ccde0094564d69dd536d6089f87647970
Dan Hiester <dan.hiester@gmail.com>
2018-05-02 07:51:55 -0700

    Remove Title from logo (the name always accompanies the logo)

--- components/DyLogo.vue
+++ components/DyLogo.vue
@@ -1,6 +1,6 @@
 <template>
   <svg width="46px" height="34px" viewBox="0 0 46 34" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>Distantly Yours Logo</title>
+    <title></title>
     <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
         <path class="shape-overlap" d="M18.7088333,30.7511815 L18.7088333,16.7680431 L26.5261291,12.049282 C27.0189929,13.5441235 27.2837153,15.1280699 27.2837153,16.7679992 C27.2837153,22.6676203 23.8577049,27.8427163 18.7088333,30.7511815 Z" :fill="overlapFill"></path>
         <path class="shape-d" d="M9.09457175,33.2372483 L2.84217094e-14,33.2372483 L2.84217094e-14,0.298750004 L9.09457175,0.298750004 C19.1401583,0.298750004 27.2837153,7.67228402 27.2837153,16.7679992 C27.2837153,25.8637143 19.1401583,33.2372483 9.09457175,33.2372483 Z" :fill="dFill" :style="dStyle"></path>
