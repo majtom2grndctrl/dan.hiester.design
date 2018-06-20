099ced30fb41758e457e655fa323aaa10019012e
Dan Hiester <dan.hiester@gmail.com>
2018-06-20 11:51:17 -0500

    Resolves #3 Populate generated blog content with payload (and streamline async content retrieval)

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>{{ post.title }}</h1>
+    <article v-html="post.content" />
+  </div>
+</template>
+
+
+<script>
+import Prismic from 'prismic-javascript'
+import PrismicDOM from 'prismic-dom'
+import { apiEndpoint, parseResponse } from './index'
+
+export default {
+  async asyncData (ctx) {
+    console.log ('ctx.params.slug = ', ctx.params.slug)
+    if (ctx.payload) {
+      console.log('payload = ', ctx.payload)
+      return { post: ctx.payload }
+    } else return Prismic.getApi(apiEndpoint).then( function (api) {
+      return api.query(
+        Prismic.Predicates.at('my.blog_post.slug', ctx.params.slug),
+      ).then( function (response) {
+        return { post: parseResponse(response) }
+      }, (err) => {
+        console.log('Something went wrong: ', err)
+        return { title: err }
+      })
+    })
+  }
+}
+</script>
