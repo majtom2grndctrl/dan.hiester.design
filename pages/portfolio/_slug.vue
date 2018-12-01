9a2a52aea56b44cce4e02aead0f2bc17037baefb
Dan Hiester <dan.hiester@gmail.com>
2018-12-01 15:30:29 -0800

    Updates #2 Load case study from CMS and styling.

--- pages/portfolio/_slug.vue
+++ pages/portfolio/_slug.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <h1 v-html="headline" />
-    <div v-html="content" />
+    <header class="caseStudy-header">
+      <h1 v-html="headline" class="caseStudy-title" />
+    </header>
+    <div v-html="content" class="prismic-content" />
   </div>
 </template>
 
@@ -13,17 +15,21 @@ import PrismicDOM from 'prismic-dom'
 
 export const parsers = {
   content (block) {
-    return (`${PrismicDOM.RichText.asHtml(block.title1)}${PrismicDOM.RichText.asHtml(block.lede)}${PrismicDOM.RichText.asHtml(block.content)}`)
+    let contentString = ''
+    !!block.title1 && (contentString += PrismicDOM.RichText.asHtml(block.title1))
+    !!block.lede && (contentString += `<div class="lede">${PrismicDOM.RichText.asHtml(block.lede)}</div>`)
+    !!block.content && (contentString += PrismicDOM.RichText.asHtml(block.content))
+    return contentString
   },
   image (block) {
 
     console.log('Current image block is: ', block)
-    return (`
+    return `
       <figure>
         <img src="${block.image.url}" alt="${block.image.alt}" />
         <div>${PrismicDOM.RichText.asHtml(block.caption)}</div>
       </figure>
-    `)
+    `
   }
 }
 
@@ -31,10 +37,9 @@ export default {
   async asyncData (ctx) {
 //    console.log ('ctx.params.slug = ', ctx.params.slug)
     if (ctx.payload) {
-     console.log('payload = ', ctx.payload)
+//      console.log('payload = ', ctx.payload)
       return { data: ctx.payload.data }
     } else return Prismic.getApi('https://distantly-yours-blog.cdn.prismic.io/api/v2').then( function (api) {
-      console.log('ctx.params.slug = ', ctx.params.slug);
       return api.query(
         Prismic.Predicates.at('my.case_study.uid', ctx.params.slug),
       ).then( function (response) {
@@ -47,17 +52,40 @@ export default {
             case 'image_block': return parsedContent = parsedContent + parsers.image(slice.primary)
           }
         })
-        console.log('data = ', data)
-        console.log('parsedContent = ', parsedContent)
+//        console.log('data = ', data)
+//        console.log('parsedContent = ', parsedContent)
         return {
           headline: PrismicDOM.RichText.asText(data.headline),
           content: parsedContent
         }
       }, (err) => {
-        console.log('Something went wrong: ', err)
+        console.error('Something went wrong: ', err)
         return { title: err }
       })
     })
   }
 }
 </script>
+
+<style lang="scss" scoped>
+  .caseStudy-header {
+    background: $bg-blue-400;
+    color: $white;
+    margin: 0 0 3rem 0;
+    overflow: hidden;
+  }
+  .caseStudy-title {
+    color: $white;
+    font-size: (24rem/16);
+    line-height: 1.4em;
+    margin: 1rem 1rem;
+  }
+  @media (min-width: $viewport-medium) {
+    .caseStudy-header {
+    }
+    .caseStudy-title {
+      font-size: (42rem/16);
+      line-height: (52rem/16);
+    }
+  }
+</style>
