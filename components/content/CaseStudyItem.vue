47a6fbbe4365ab38b95fb0a3e67368a9acf1db1c
Dan Hiester <dan.hiester@gmail.com>
2019-03-09 20:07:12 -0800

    Begin stubbing out a component for an item in a list of Portfolio projects.

---
+++ components/content/CaseStudyItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="caseSudyItem">
+    <img src="thumbnail.src" alt="thumbnail.alt" />
+  </div>
+</template>
+
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+export interface ICaseStudyItem {
+  title: string;
+  categories: [string];
+  img: {
+    src: string;
+    alt: string;
+  }
+}
+
+@Component({})
+class CaseStudyItem extends Vue {
+  @Prop()
+  details: ICaseStudyItem
+}
+
+export default CaseStudyItem
+</script>
+
