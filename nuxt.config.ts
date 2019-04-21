f988a43eb445ad868fd21e1bd612d10cba651e12
Dan Hiester <dan.hiester@gmail.com>
2019-04-21 13:46:31 -0700

    Make blog DX still work without wifi. 🚌🎶

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,4 @@
+import { blogDataMock } from './dataMocks'
 const Prismic = require('prismic-javascript')
 const PrismicDOM = require('prismic-dom')
 
@@ -61,11 +62,30 @@ module.exports = {
   css: ['~/assets/html.css'],
   generate: {
     routes: async function () {
-      const api = await Prismic.getApi('https://distantly-yours-blog.cdn.prismic.io/api/v2')
-      const blogQuery = await api.query(Prismic.Predicates.at('document.type', 'blog_post'), { orderings: '[my.blog_post.date desc]' })
-      console.log('blog_posts = ', blogQuery.results)
-      const portfolioQuery = await api.query(Prismic.Predicates.at('document.type', 'case_study'), {} )
-      console.log('Case Studies fetched: ', portfolioQuery.results)
+      const apiUrl = 'https://distantly-yours-blog.cdn.prismic.io/api/v2'
+      const blogQuery = await Prismic.getApi(apiUrl)
+        .then( api =>  {
+          return api.query(
+            Prismic.Predicates.at('document.type', 'blog_post'), { orderings: '[my.blog_post.date desc]' }
+          )
+        })
+        .catch(err => {
+          console.warn('Hey, something happened to the network.', err)
+          // If we’re in dev mode, return a data mock. Otherwise, return null and force an error.
+          return process.env.NODE_ENV === 'development' ? { blogDataMock } : { results: [] }
+        })
+      console.log('blogQuery = ', blogQuery)
+      const portfolioQuery = await Prismic.getApi(apiUrl)
+        .then( api => { 
+          return api.query(
+            Prismic.Predicates.at('document.type', 'case_study'), null 
+          )
+        }).catch(err => {
+          console.warn('Hey, something happened to the network.', err)
+          // If we’re in dev mode, return a data mock. Otherwise, return null and force an error.
+          return process.env.NODE_ENV === 'development' ? { results: [] } : null
+        })
+      console.log('portfolioQuery = ', portfolioQuery)
       const routesList = [
         {
           route: '/blog',
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,34 +1,50 @@
 <template>
-  <section class="blogIndex">
+  <section class="blogIndex" v-if="blog_posts">
     <article class="blogPost" v-for="(post, index) in blog_posts" :key="index">
       <h2><nuxt-link :to="post.url" v-html="post.title" /></h2>
       <div v-html="post.content" />
     </article>
   </section>
+  <section v-else>
+    <h1>Signal disrupted</h1>
+    <p>Your browser tried to download this blog post, but something went wrong. Try checking your internet connection and <button onClick="window.location.reload()" class="btn-link">refreshing this page</button>.</p>
+  </section>
 </template>
 
 
-<script>
-import { swipeTransition } from '~/components/layout/MainLayout'
+<script lang="ts">
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+import { swipeTransition } from '~/components/layout/MainLayout.vue'
 import Prismic from 'prismic-javascript'
 import PrismicDOM from 'prismic-dom'
+import ApiSearchResponse from 'prismic-javascript/d.ts/ApiSearchResponse'
+import { Document } from 'prismic-javascript/d.ts/documents'
+import { blogDataMock } from '~/dataMocks'
 
 export const apiEndpoint = 'https://distantly-yours-blog.cdn.prismic.io/api/v2'
 
-export function parseResponse (response) {
-  function parseOne(result) {
-    return {
+export interface BlogPostData {
+  uid?: string;
+  url: string;
+  title: string;
+  content: string;
+}
+
+export function parseResponse (response: ApiSearchResponse) {
+  function parseOne(result: Document) {
+    const output: BlogPostData = {
       url: '/blog/' + result.uid,
       title: PrismicDOM.RichText.asText(result.data.title),
       content: PrismicDOM.RichText.asHtml(result.data.body)
     }
+    return output
   }
 
 //  console.log('blog_posts = ', response.results)
   if (response.results.length === 1) {
     return parseOne(response.results[0])
-  } else {
-    const parsed_posts = []
+  } else if (response.results.length > 1) {
+    const parsed_posts: BlogPostData[] = []
     response.results.map((result) => {
       parsed_posts.push(parseOne(result))
     })
@@ -36,7 +52,8 @@ export function parseResponse (response) {
   }
 }
 
-export default {
+@Component({})
+class BlogIndex extends Vue {
   async asyncData (ctx) {
     if (ctx.payload) return { blog_posts: ctx.payload }
     else return Prismic.getApi(apiEndpoint).then( function (api) {
@@ -49,12 +66,17 @@ export default {
         console.log('Something went wrong: ', err)
         return { title: err }
       })
+    }).catch( err => {
+      console.warn("Error downloading posts")
+      return process.env.NODE_ENV === 'development' ? { blog_posts: parseResponse(blogDataMock) } : { blog_posts: undefined }
     })
-  },
+  }
   transition (to, from) {
     return swipeTransition(to, from)
   }
 }
+
+export default BlogIndex
 </script>
 
 
