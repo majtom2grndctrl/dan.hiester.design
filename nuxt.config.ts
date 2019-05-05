6a216a55394bf5ad42f362dc7e33f45e7e093457
Dan Hiester <dan.hiester@gmail.com>
2019-05-05 01:15:52 -0700

    Add support for custom media PostCSS plugin.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,20 +41,11 @@ module.exports = {
   ** Build configuration
   */
   build: {
-    /*
-    ** Run ESLint on save
-    */
-   /*
-    extend (config, ctx) {
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
+    postcss: {
+      plugins: {
+        'postcss-custom-media': {},
       }
-    }*/
+    }
   },
   styleResources: {
     sass: ['~/assets/main.scss']
