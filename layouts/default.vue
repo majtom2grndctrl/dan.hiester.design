694308721135cbbc845aec80f143d18ab5ffb89d
Dan Hiester <dan.hiester@gmail.com>
2019-05-06 21:32:35 -0700

    Hide flyout menu on interior site nave, add “Push Farther” to tablet-ish sized header.

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,35 +32,9 @@ export default Vue.extend ({
 </script>
 
 <style lang="scss">
-.push-farther {
-  display: none;
-  @media (min-width: $viewport-medium) {
-    color: #797979;
-    display: block;
-    font-weight: 300;
-    font-size: (15rem/16);
-    letter-spacing: .05ex;
-    opacity: .6;
-    padding: 0;
-    position: absolute;
-      top: (48rem/16);
-      right: (100% * 1/14);
-    text-transform: uppercase;
-    transition: transform .5s ease, opacity .5s, filter .5s;
-    &:before {
-      color: #076381;
-      content: '//';
-      font-size: (26em/15);
-      font-weight: bold;
-      font-style: italic;
-      mix-blend-mode: color-burn;
-      padding: 0 .125em;
-      position: absolute;
-        left: -(20em/15);
-        top: 45%;
-      transform: translate(0, -50%);
-    }
-  }
+// This prevents major CSS breakage.
+.unused-class {
+  color: $bg-blue-400;
 }
 
 </style>
