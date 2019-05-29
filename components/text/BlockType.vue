a424a540d29b3b6e5fa2fbe9769d767e738cb635
Dan Hiester <dan.hiester@gmail.com>
2019-05-28 19:34:56 -0700

    Initial implementation of block type component.

---
+++ components/text/BlockType.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="BlockType"><slot /></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
+
+
+<style lang="postcss" scoped>
+  .BlockType {
+    color: var(--white);
+    font-size: calc(14rem/16);
+    font-weight: 500;
+    line-height: 1em;
+    margin: 0 0 calc(5rem/16) 0;
+    text-transform: uppercase;
+  }
+</style>
