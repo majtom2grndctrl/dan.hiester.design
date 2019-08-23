384a2884fbd3046946fe709d36e7c565a8b40fd0
Dan Hiester <dan.hiester@gmail.com>
2019-08-23 08:33:56 -0700

    Add a component for reading and displaying content blocks.

---
+++ components/content/ContentSlice.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="prismic-content">
+    <div v-if="title1" v-html="title1" />
+    <div class="lede" v-if="lede" v-html="lede" />
+    <div v-if="content" v-html="content" />
+  </div>
+</template>
+
+<script lang="ts">
+import PrismicDOM from 'prismic-dom';
+import { createComponent, value, onMounted } from 'vue-function-api';
+import BlockType from '~/components/text/BlockType.vue';
+
+const ContentSlice = createComponent({
+  props: {
+    block: {
+      block_type: String,
+      title1: String,
+      lede: String,
+      content: String,
+    }
+  },
+  setup(props) {
+    const block = props.block!;
+    const blockType = value(block.block_type);
+    const title1 = value(PrismicDOM.RichText.asHtml(block.title1));
+    const lede = value(PrismicDOM.RichText.asHtml(block.lede));
+    const content = value(PrismicDOM.RichText.asHtml(block.content));
+
+    return {
+      blockType,
+      title1,
+      lede,
+      content,
+    };
+  },
+});
+
+export default ContentSlice;
+</script>
+
+<style lang="postcss" scoped>
+  .block-type {
+    color: var(--gray-600);
+  }
+</style>
+
--- components/content/PrismicSlices.vue
+++ components/content/PrismicSlices.vue
@@ -2,6 +2,7 @@
   <div class="prismic-content">
   <template v-for="(slice, i) in prismicDocument.data.body">
     <ImageSlice v-if="slice.slice_type == 'image_block'" :block="slice.primary" :key="'image-' + i" />
+    <ContentSlice v-if="slice.slice_type == 'content_block'" :block="slice.primary" :key="'content-' + i" />
   </template>
   <div class="prismic-content" v-html="parsedContent" />
   </div>
@@ -13,29 +14,11 @@ import { Vue, Component, Prop } from 'nuxt-property-decorator'
 import PrismicDOM from 'prismic-dom'
 import { Document } from 'prismic-javascript/d.ts/documents';
 import ImageSlice from '~/components/content/ImageSlice.vue';
-
-const parsers = {
-  content (block) {
-    let contentString = ''
-    !!block.title1 && (contentString += PrismicDOM.RichText.asHtml(block.title1))
-    !!block.lede && (contentString += `<div class="lede">${PrismicDOM.RichText.asHtml(block.lede)}</div>`)
-    !!block.content && (contentString += PrismicDOM.RichText.asHtml(block.content))
-    return contentString
-  }
-}
-
-function parseSliceContent(prismicDocument: Document) {
-  let parsedContent = ''
-  prismicDocument && prismicDocument.data.body.map( slice => {
-    switch(slice.slice_type) {
-      case 'content_block': return parsedContent = parsedContent + parsers.content(slice.primary)
-    }
-  })
-  return parsedContent
-}
+import ContentSlice from '~/components/content/ContentSlice.vue';
 
 @Component({
   components: {
+    ContentSlice,
     ImageSlice,
   },
 })
@@ -47,7 +30,6 @@ class PrismicSlices extends Vue {
     // console.log('Document is: ', this.$props.prismicDocument)
     return {
       slices: this.prismicDocument.data.body,
-      parsedContent: parseSliceContent(this.prismicDocument),
     }
   }
 
