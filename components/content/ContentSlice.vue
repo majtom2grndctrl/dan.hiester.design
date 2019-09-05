be6df4bae4c141232e226c9102c7bbc22e86c99e
Dan Hiester <dan.hiester@gmail.com>
2019-09-05 07:30:19 -0700

    Resolves #31 Update header font to Gilmer
    But leave Poppins as a fallback, for now.

--- components/content/ContentSlice.vue
+++ components/content/ContentSlice.vue
@@ -91,6 +91,7 @@ export default ContentSlice;
   }
   & h2 {
     font-size: var(--type-scale-3);
+    font-weight: 500;
     line-height: var(--spatial-scale-7);
     margin-top: 0;
     margin-bottom: 0;
