b32aa4102f908b6320e1655d44ed29a680c95c81
Dan Hiester <dan.hiester@gmail.com>
2019-11-22 00:12:18 -0500

    Eliminate an invalid HTML attribute.

--- components/content/BlogPost.vue
+++ components/content/BlogPost.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="BlogPost" :style="`background: ${post.indexBgColor};`" ref="rootElement" v-on:click="this.handleTileClick">
+  <div class="BlogPost" :style="`background: ${post.indexBgColor};`" v-on:click="this.handleTileClick">
     <!--<img src="imgSrc" alt="imgAlt" />-->
     <div class="text">
       <h2 class="title" v-html="post.title" />
